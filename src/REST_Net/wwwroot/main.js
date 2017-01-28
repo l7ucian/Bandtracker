60855128343f999e251ed5c620b30375511d9127
Andercou Lucian <lucian.andercou@gmail.com>
2017-01-28 13:25:45 +0200

    Added the static files to the project

---
+++ src/REST_Net/wwwroot/main.js
@@ -0,0 +1,33 @@
+var app = angular.module('bandtracker', ['ngResource']);
+
+app.config(function($httpProvider, $resourceProvider){
+    $resourceProvider.defaults.stripTrailingSlahes = false;
+});
+
+app.controller('BandData', function ($scope, $http) {
+    $scope.find = function () {
+        $http.get('http://localhost:23244/api/values').
+            success(function (response) {
+                json = JSON.parse(response);
+                $scope.locations = json.data;
+                if (typeof $scope.locations == 'object')
+                    console.log($scope.locations);
+                else
+                    console.log('Not actually an object, it really is ', typeof $scope.greeting);
+            });
+    };
+});
+
+app.controller('BandsController', function ($scope) {
+$scope.persons= [
+			{
+				"name": "Gregory Huffman",
+				"email": "Praesent@pedenec.net",
+				"birthdate": "2015-03-23T18:00:37-07:00",
+				"phonenumber": "07624 073918",
+				"address": "5880 Sed, Street",
+				"city": "Denderbelle",
+				"country": "Ethiopia"
+			}
+		]
+});
